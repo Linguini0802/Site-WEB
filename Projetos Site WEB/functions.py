873d759396e02d2ed4819428721873073ff87938
Linguini0802 <viniciuslunardifp@gmail.com>
2026-08-28 19:17:07 -0300

    Add files via upload

---
+++ Projetos Site WEB/functions.py
@@ -0,0 +1,24 @@
+import os, json
+
+
+caminho = r"C:\Users\Vini\OneDrive\Desktop\Programação\Projetos Site WEB\cadastros.json"
+
+def salvar_dados(cadastro):
+    '''salvar_dados: Ao chamar a função, ele vai pegar o dicionário informado no parâmetro e mudar ou criar um arquivo .json para armazenar
+    as informações do dicionário no arquivo tarefas.json'''
+    with open(caminho, "w", encoding="utf-8") as arquivo:
+        json.dump(cadastro, arquivo, indent=4)
+
+
+def carregar_dados():
+    '''carregar_dados: Ao chamar a função, ele vai puxar como um dicionário as informações contidas no arquivo tarefas.json,
+    se não existir, ele retorna que não existe'''
+    try:
+        with open(caminho, "r", encoding="utf-8") as arquivo:
+            return json.load(arquivo)
+    except FileNotFoundError:
+        return {"Linguini0802": "Vini@0802"}
+
+
+def cadastrar_usuarios(usuario, senha):
+    '''cadastrar_usuarios: Ao chamar a função, ele vai inserir o usuário e a senha informada no cadastros.json'''
