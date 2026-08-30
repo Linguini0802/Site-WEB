436bf747dcdf6490adb920148b934d1c4ce71e8c
Vinicius Lunardi <viniciuslunardifp@gmail.com>
2026-08-30 16:07:05 -0300

    Add files via upload
    Adicionado a funcionalidade de editar o nome e senha do usuário

--- views.py
+++ views.py
@@ -26,13 +26,55 @@ def login():
     return render_template('login.html', erro=mensagem_erro)
 
 #ÁREA ADM
-@app.route('/admin', methods=['GET'])
+@app.route('/admin', methods=['POST', 'GET'])
 def admin():
     dados_cadastros = functions.carregar_dados()
     stats_delete = request.args.get('stats_delete')
-    
+
     # Envia os dados com a chave 'usuarios' para o HTML
     return render_template('admin.html', usuarios=dados_cadastros, stats_delete=stats_delete)
+
+#ÁREA ADM/ EDITAR USUARIO
+@app.route('/admin/edit_user/<usuario>', methods=['POST', 'GET'])
+def edit_user(usuario):
+
+    stats_edit = False
+    cadastros = functions.carregar_dados()
+
+    if request.method == 'GET':
+        for u in cadastros:
+            if u == usuario:
+                senha = cadastros[u]
+                return render_template('edit_user.html', usuario=usuario, senha=senha)
+
+    if request.method == 'POST':
+        usuario_editado = request.form.get('edit_name')
+        senha_editada = request.form.get('edit_password')
+        senha = cadastros[usuario]
+        if not senha_editada:
+            for u in cadastros:
+                if u == usuario:
+                    senha_editada = cadastros[u]
+
+        print(f'a {senha_editada}')
+        if usuario_editado in cadastros:
+            mensagem_erro = "Usuário já cadastrado!"
+            return render_template('edit_user.html', usuario=usuario, senha=senha, erro=mensagem_erro)
+        
+        elif len(senha_editada) <= 7 or not any(char.isdigit() for char in senha_editada):
+            mensagem_erro = "A senha deve conter ao mínimo 8 caracteres e ao menos um número!"
+            return render_template('edit_user.html', usuario=usuario, senha=senha, erro=mensagem_erro)
+        
+        else:
+            for u in cadastros:
+                if u == usuario:
+                    del cadastros[usuario]
+                    cadastros[usuario_editado] = senha_editada
+                    functions.salvar_dados(cadastros)
+                    stats_edit = True
+                    return redirect(url_for('admin', stats_edit=stats_edit))
+
+    return render_template('edit_user.html')
 
 #ÁREA ADM/ DELETAR USUÁRIO
 @app.route('/admin/delete_user', methods=['POST', 'GET'])
@@ -54,7 +96,6 @@ def delete_user():
 #CADASTRAR
 @app.route('/cadaster', methods=['GET', 'POST'])
 def cadaster():
-   
    
     if request.method == 'POST':
         cadastro_usuario = request.form.get('cadastro_usuario')
